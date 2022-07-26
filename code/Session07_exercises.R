b56cef543831ab113f84de9b4c7ac1cc9181a45c
Joelle Mbatchou <joelle.mbatchou@regeneron.com>
2022-07-26 17:13:45 +0000

    edit r codes

--- code/Session07_exercises.R
+++ code/Session07_exercises.R
@@ -6,32 +6,41 @@ require(SKAT)
 require(ACAT)
 require(ggplot2)
 
+## Set to your home directory if on server
+setwd("~/")
+
+## Data sets are in : /data/SISG2022M15/data/
+
 ###########################
 ## Rare Variant Analysis ##
 ###########################
 # Question 1. Using PLINK, extract **rare variants** in a new PLINK BED file.
-system("plink2 --bfile <plink_bed_prefix> --max-maf .. --maj-ref force --out <output_prefix>")
+# PLINK BED file is in /data/SISG2022M15/data/rv_geno_chr1.bed
+system("/data/SISG2022M15/exe/plink2 --bfile <bed_prefix> --max-maf 0.05 --maj-ref force --make-bed --out <output_prefix>")
 
 
 # Question 2. Load the data in R
-## Read in the SNPs using R function `BEDMatrix()` (hint: use option `simple_names = TRUE` to easily filter by sample IID later)
+## Read in the SNPs from the BED file created in Q1 using R function `BEDMatrix()
+G <- BEDMatrix( "<bed_Q1_prefix>", simple_names = TRUE) %>% as.matrix
+iid.geno <- rownames(G) # sample IDs of individuals in the genotype data
 
 ## Load the phenotype data from `rv_pheno.txt`
+y <- fread("<pheno_file>", header = TRUE)
 
 ## Keep only samples who are present both in the genotype as well as phenotype data and who don't have missing values for the phenotype
 
-
 # Question 3: Examine the genotype data:
 ## Compute the minor allele frequency (MAF) for each SNP and plot histogram. (hint: use `na.rm=TRUE` when calling `mean()`)
+maf <- colMeans(G, na.rm = TRUE)/2
 
 ## Check for missing values
 
 
 # Question 4: Run the single variant association tests in PLINK (only for the extracted variants).
-system("plink2 --bfile <BED_file_with_extracted_SNPs> --pheno rv_pheno.txt --pheno-name <pheno_name> --glm allow-no-covars --out <output_prefix>")
+system("/data/SISG2022M15/exe/plink2 --bfile <BED_file_with_extracted_SNPs> --pheno rv_pheno.txt --pheno-name <pheno_name> --glm allow-no-covars --out <output_prefix>")
 ## What would be your significance threshold after applying Bonferroni correction for the multiple tests (assume the significance level is 0.05)? Is anything significant after this correction?
 
-## Make a volcano plot (i.e. log10 p-values vs effect sizes).
+## Make a volcano plot (i.e. log10 p-values (y) vs effect sizes (x) ).
 
 
 # Question 5: We will first compare three collapsing/burden approaches:
@@ -44,7 +53,7 @@ burden.mz <- ..
 lm( <y> ~ burden.mz) %>% summary
 
 ## Weighted burden test: for each individual, take a weighted count of the rare alleles across sites
-weights <- dbeta( <MAF_vector> , 1, 25)
+weights <- dbeta( maf , 1, 25)
 burden.weighted <- ..
 lm( <y> ~ burden.weighted) %>% summary
 
@@ -63,7 +72,7 @@ SKAT( <genotype_matrix>, skat.null, method="optimal.adj")
 
 
 # Question 9: Run ACATV on the single variant p-values.
-acat.weights <- weights * weights * <MAF> * (1 - <MAF>)
+acat.weights <- weights * weights * maf * (1 - maf)
 ACAT( <pvalues>, weights = acat.weights)
 
 
--- code/Session08_exercises.R
+++ code/Session08_exercises.R
@@ -5,6 +5,11 @@ require(GWASTools)
 require(ggplot2)
 require(patchwork)
 
+## Set to your home directory if on server
+setwd("~/")
+
+## Data sets are in : /data/SISG2022M15/data/
+
 ####################################
 ## Case-control imbalance in GWAS ##
 ####################################
@@ -12,14 +17,14 @@ require(patchwork)
 #### Data simulation - genotypes
 N <- 10e3
 # Generate a configuration file specifying allele frequencies (a,b) for Uniform(a,b) distribution
-write(paste0("5000 common 0.05 0.5 1 1"), "tmp/sim.config")
-write(paste0("5000 rare 0.001 0.01 1 1"), "tmp/sim.config", append = TRUE)
+write(paste0("5000 common 0.05 0.5 1 1"), "sim.config")
+write(paste0("5000 rare 0.001 0.01 1 1"), "sim.config", append = TRUE)
 # Run PLINK1.9
-system(paste0("plink1.9 --make-bed --simulate tmp/sim.config --simulate-ncases ", N, " --simulate-ncontrols 0 --simulate-prevalence 0.1  --out tmp/cc_imb_geno"))
+system(paste0("/data/SISG2022M15/exe/plink1.9 --make-bed --simulate sim.config --simulate-ncases ", N, " --simulate-ncontrols 0 --simulate-prevalence 0.1  --out cc_imb_geno"))
 
 #### Data simulation - phenotypes
 # get FID/IID from FAM file
-sample.ids <- fread("tmp/cc_imb_geno.fam", header = FALSE)
+sample.ids <- fread("cc_imb_geno.fam", header = FALSE)
 ## Set prevalence = 10% (CCR 1:9)
 y1 <- rbinom(N, 1, prob = 0.1 )
 ## Set prevalence = 1% (CCR 1:99)
@@ -28,21 +33,23 @@ y2 <- rbinom(N, 1, prob = 0.01 )
 y3 <- rbinom(N, 1, prob = 0.005 )
 # write to file
 data.frame(FID = sample.ids$V1, IID = sample.ids$V2, Y1 = y1, Y2 = y2, Y3 = y3) %>%
-  fwrite("tmp/cc_imb_pheno.txt", sep = "\t", na = NA, quote = FALSE)
+  fwrite("cc_imb_pheno.txt", sep = "\t", na = NA, quote = FALSE)
 
 
 
-# Question 1.  Run GWAS in REGENIE (step 2) analyzing all 3 traits as quantitative.
-system("regenie --bed tmp/cc_imb_geno --phenoFile tmp/cc_imb_pheno.txt --step 2 --bsize 400 --qt  --ignore-pred --out <output_prefix>")
+# Question 1. Run GWAS in REGENIE (step 2 only) analyzing all 3 traits as quantitative.
+system("/data/SISG2022M15/exe/regenie --bed cc_imb_geno --phenoFile cc_imb_pheno.txt --step 2 --bsize 400 --qt --ignore-pred --out <output_prefix>")
 
 
 # Question 2. Read in the three summary statistics files in R and make a QQ plot of the p-values for each phenotype. Since these are null SNPs, how does it compare to what we expect?
-## Read in the SNPs using R function `BEDMatrix()` (hint: use option `simple_names = TRUE` to easily filter by sample IID later)
-sumstats.y1 <- fread("<output_prefix>_Y1.regenie") %>% mutate(Pval = 10^(-LOG10P), Z = sign(BETA) * sqrt(CHISQ))
+sumstats.y1 <- fread("<output_prefix>_Y1.regenie") %>% 
+  mutate(Pval = 10^(-LOG10P), Z = sign(BETA) * sqrt(CHISQ)) # get Pvalues & Z-scores
 qqPlot( pval = sumstats.y1$Pval )
+# do for remaining traits as well (Y2, Y3)
 
 
 # Question 3: Make a histogram of the test statistics for each phenotype and overlay with a normal distribution. How well do they match? What do you observe as the case-control imbalance gets more severe?
+
 ## R function to easily make this plot for different phenotypes.
 plot.sumstats.hist <- function(df, title = ""){
   df %>%
@@ -56,12 +63,14 @@ plot.sumstats.hist <- function(df, title = ""){
     labs(title = title)
 }
 plot.sumstats.hist(sumstats.y1, title = "Y1")
+# do for remaining traits as well (Y2, Y3)
 
 
 # Question 4: Re-do 3 but now separate the histogram for common and rare SNPs. 
 ## First separate the data frame based on common/rare simulated SNPs.
 sumstats.y1.common <- sumstats.y1[ grepl("common", ID), ]
 sumstats.y1.rare <- sumstats.y1[ grepl("rare", ID), ]
+# do for remaining traits as well (Y2, Y3)
 
 ## Make a histogram of the test statistics distribution at common/rare SNPs. What do you observe across the different case-control imbalances? 
 plot.sumstats.hist(sumstats.y1.common, title = "Y1 - Common SNPs") | plot.sumstats.hist(sumstats.y1.rare, title = "Y1 - Rare SNPs")
